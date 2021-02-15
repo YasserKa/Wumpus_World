04226ab5c85e4702928c214e8a523bbadbe4bbfb
Yasser Kaddoura <yasser.kaddoura19@gmail.om>
2021-02-15 11:40:00 +0100

    formatting: removal of tabs from files + add new simple map in maps

--- src/main/java/wumpus/Agent.java
+++ src/main/java/wumpus/Agent.java
@@ -2,26 +2,26 @@ package wumpus;
 
 public abstract class Agent
 {
-	
-	// Actuators
-	public enum Action
-	{
-		TURN_LEFT,
-		TURN_RIGHT,
-		FORWARD,
-		SHOOT,
-		GRAB,
-		CLIMB
-	}
-	
-	public abstract Action getAction
-	(
-		// Sensors
-		boolean stench,
-		boolean breeze,
-		boolean glitter,
-		boolean bump,
-		boolean scream
-	);
-	
+
+    // Actuators
+    public enum Action
+    {
+        TURN_LEFT,
+        TURN_RIGHT,
+        FORWARD,
+        SHOOT,
+        GRAB,
+        CLIMB
+    }
+
+    public abstract Action getAction
+        (
+         // Sensors
+         boolean stench,
+         boolean breeze,
+         boolean glitter,
+         boolean bump,
+         boolean scream
+        );
+
 }
